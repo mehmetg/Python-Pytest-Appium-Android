7ef13f2c1153f14deeca57107e7423c37f9188b6
Mehmet Gerceker <mehmetg@saucelabs.com>
2015-12-15 23:46:46 -0800

    refactored and ready to test in CI virtualenv should be used in CI

---
+++ tests/sauceutils.py
@@ -0,0 +1,32 @@
+from __future__ import print_function
+import os
+import requests
+import hashlib
+
+
+def upload_app(app_path, username, password):
+    app_file_name = os.path.basename(app_path)
+    src_md5 = md5(app_path)
+    resp = None
+    with open(app_path, 'rb') as f:
+        resp = requests.post(
+                url='https://saucelabs.com/rest/v1/storage/%s/%s?overwrite=true' % (username, app_file_name),
+                data=f,
+                auth=(username, password),
+                headers={'Content-Type': 'application/octet-stream'})
+    if resp.status_code != 200:
+        print(resp, out=sys.stderr)
+        raise Exception("Upload failed!")
+    else:
+        if resp.json()['md5'] == src_md5:
+            print("File uploaded successfully!")
+        else:
+            raise Exception("Upload failed: File corrupted!")
+
+
+def md5(file_path):
+    hash = hashlib.md5()
+    with open(file_path, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash.update(chunk)
+    return hash.hexdigest()
